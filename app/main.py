25a74e15156c9cac749ac2dd9bf4cb06b37d2172
ESergievich <sergievichevgeniy@gmail.com>
2025-11-11 14:03:13 +0300

    feat: add lifespan context manager to handle database disposal

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
+
 from api import router as api_router
 from fastapi import FastAPI
 
-app = FastAPI(title="Business management system API")
+from app.core.db_helper import db_helper
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:  # noqa: ARG001
+    yield
+    await db_helper.dispose()
+
+
+app = FastAPI(title="Business management system API", lifespan=lifespan)
 
 app.include_router(api_router)
 
