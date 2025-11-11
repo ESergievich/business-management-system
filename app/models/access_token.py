e465310f24bb28a87953138a2fde01af9efe292d
ESergievich <sergievichevgeniy@gmail.com>
2025-11-11 03:22:45 +0300

    feat: add AccessToken model for FastAPI-Users

---
+++ app/models/access_token.py
@@ -0,0 +1,17 @@
+from fastapi_users.authentication.strategy import AccessTokenProtocol
+from fastapi_users_db_sqlalchemy.access_token import SQLAlchemyBaseAccessTokenTable
+from sqlalchemy import ForeignKey, Integer
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.models.base import Base
+
+
+class AccessToken(Base, SQLAlchemyBaseAccessTokenTable[int], AccessTokenProtocol[int]):
+    """Access token model."""
+
+    id = None  # type: ignore[assignment]
+    user_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey("users.id", ondelete="cascade"),
+        nullable=False,
+    )
