ce615aae045382b967d3c840d8b0723ba5c58cee
ESergievich <sergievichevgeniy@gmail.com>
2025-11-11 04:35:27 +0300

    feat: create get_user_manager dependency

--- app/authentication/dependencies.py
+++ app/authentication/dependencies.py
@@ -5,11 +5,13 @@ from fastapi import Depends
 from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 from fastapi_users_db_sqlalchemy.access_token import SQLAlchemyAccessTokenDatabase
 
+from app.authentication.user_manager import UserManager
 from app.core.db_helper import db_helper
 from app.models.access_token import AccessToken
 from app.models.user import User
 
 if TYPE_CHECKING:
+    from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase as SQLAlchemyUserDatabaseType
     from sqlalchemy.ext.asyncio import AsyncSession
 
 
@@ -23,3 +25,9 @@ async def get_access_tokens_db(
     session: Annotated["AsyncSession", Depends(db_helper.session_getter)],
 ) -> AsyncGenerator[SQLAlchemyAccessTokenDatabase[AccessToken], None]:
     yield SQLAlchemyAccessTokenDatabase(session, AccessToken)
+
+
+async def get_user_manager(
+    users_db: Annotated["SQLAlchemyUserDatabaseType[User, int]", Depends(get_user_db)],
+) -> AsyncGenerator[UserManager, None]:
+    yield UserManager(users_db)
